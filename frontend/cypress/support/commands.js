f9bf3b0a5da281dd2369d1cbcc361f1b50272ab8
MJONES155 <mjones155@murraystate.edu>
2025-11-22 00:04:23 -0600

    ESLint(cypress): declare globals, single quotes, EOF newline

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -1,3 +1,5 @@
+/* eslint-env cypress */
+/* global Cypress, cy */
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -11,33 +13,33 @@
 //
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
-Cypress.Commands.add("login", () => {
-  cy.visit("/login");
-  cy.get("[data-test=email]").type("test@example.com");
-  cy.get("[data-test=password]").type("password123");
-  cy.get("[data-test=login-button]").click();
-  cy.url().should("not.include", "/login");
+Cypress.Commands.add('login', () => {
+  cy.visit('/login');
+  cy.get('[data-test=email]').type('test@example.com');
+  cy.get('[data-test=password]').type('password123');
+  cy.get('[data-test=login-button]').click();
+  cy.url().should('not.include', '/login');
 });
 
-Cypress.Commands.add("signup", (user) => {
+Cypress.Commands.add('signup', (user) => {
   // `user` is an object with firstName, lastName, email, password
-  cy.visit("/signup");
+  cy.visit('/signup');
 
-  cy.get("[data-test=firstName]").type(user.firstName);
-  cy.get("[data-test=lastName]").type(user.lastName);
-  cy.get("[data-test=email]").type(user.email);
-  cy.get("[data-test=password]").type(user.password);
-  cy.get("[data-test=confirmPassword]").type(user.password);
+  cy.get('[data-test=firstName]').type(user.firstName);
+  cy.get('[data-test=lastName]').type(user.lastName);
+  cy.get('[data-test=email]').type(user.email);
+  cy.get('[data-test=password]').type(user.password);
+  cy.get('[data-test=confirmPassword]').type(user.password);
 
-  cy.get("[data-test=signup-button]").click();
+  cy.get('[data-test=signup-button]').click();
   // Wait until signup completes and app navigates
-  cy.url({ timeout: 20000 }).should("include", "/dashboard");
+  cy.url({ timeout: 20000 }).should('include', '/dashboard');
   // Ensure access token is stored (authentication complete)
   cy.window().then((win) => {
-    const token = win.localStorage.getItem("access_token");
-    expect(token, "access token present after signup").to.be.a("string").and.not.be.empty;
+    const token = win.localStorage.getItem('access_token');
+    expect(token, 'access token present after signup').to.be.a('string').and.not
+      .be.empty;
   });
-  
 });
 
 //
@@ -51,4 +53,4 @@ Cypress.Commands.add("signup", (user) => {
 //
 //
 // -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { /* noop placeholder */ })
